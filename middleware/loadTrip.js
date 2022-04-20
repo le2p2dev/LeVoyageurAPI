bbaccacb267a9a36a5c1c2dd21c5b427c0d55f0f
snoke1010 <48602086+snok1010@users.noreply.github.com>
2022-04-20 20:19:48 +0200

    [ADD] add middleware for the first part of the API

---
+++ middleware/loadTrip.js
@@ -0,0 +1,17 @@
+const db = require("../models");
+
+module.exports = {
+  async loadTrip(req, res, next) {
+    const trip = await db.Trip.findByPk(req.params.tripId);
+
+    if (!trip) {
+      const error = new Error("Trip not found");
+      error.code = 404;
+      return next(error);
+    }
+
+    req.trip = trip;
+
+    return next();
+  },
+};
