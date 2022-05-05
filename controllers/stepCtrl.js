b1dec6be883ef8e1865ba314afa9fc4810057775
snoke1010 <48602086+snok1010@users.noreply.github.com>
2022-05-05 18:36:42 +0200

    [UPD] update step duration

--- controllers/stepCtrl.js
+++ controllers/stepCtrl.js
@@ -47,6 +47,8 @@ module.exports = {
       return res.status(404).send("No step found");
     }
 
+    const beforeUp = req.step.duration;
+
     if (req.body.title) req.step.title = req.body.title;
 
     if (req.body.description) req.step.description = req.body.description;
@@ -61,17 +63,50 @@ module.exports = {
       const newData = await req.step.save();
       req.step = newData;
 
-      for (let index = 1; index <= req.body.duration; index++) {
-        const day = db.Day.build({
-          number: index,
-          StepId: req.step.id,
+      // nouvelle duration inferieur a ancienne
+      if (req.body.duration < beforeUp) {
+        const dif = parseInt(beforeUp, 10) - parseInt(req.body.duration, 10);
+        console.log("ici " + dif);
+        const data = await db.Day.findAll({
+          where: {
+            StepId: req.step.id,
+          },
         });
 
-        day.save();
+        console.log(data.length - dif);
+
+        for (let index = data.length; index > data.length - dif; index--) {
+          console.log("i " + index);
+          const element = data[index - 1];
+          await element.destroy();
+        }
       }
+
+      if (req.body.duration > beforeUp) {
+        const data = await db.Day.findAll({
+          where: {
+            StepId: req.step.id,
+          },
+          order: [["number", "DESC"]],
+        });
+
+        for (
+          let index = data.length;
+          index < parseInt(req.body.duration);
+          index++
+        ) {
+          const day = db.Day.build({
+            number: index + 1,
+            StepId: req.step.id,
+          });
+
+          day.save();
+        }
+      }
+
       return res.status(201).send(newData);
     } catch (err) {
-      const error = new Error("Modification failed");
+      const error = new Error("Modification failed " + err);
       error.code = 500;
       next(error);
     }
