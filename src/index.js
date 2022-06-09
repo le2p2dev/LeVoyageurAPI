65fb640795927e63ab8a82ee5c10671c586b57c4
snoke1010 <48602086+snok1010@users.noreply.github.com>
2022-06-09 09:29:44 +0200

    [ADD] add file gestion and gitigniore change

--- src/index.js
+++ src/index.js
@@ -3,24 +3,24 @@ const app = express();
 const bodyParser = require("body-parser");
 const db = require("../models");
 const cors = require('cors')
-
+const path = require("path");
 /**
  * Swagger doc
  */
 const swaggerJsDoc = require("swagger-jsdoc");
 const swaggerUI = require("swagger-ui-express");
 const swaggerOptions = {
-  swaggerDefinition: {
-    info: {
-      title: "LeVoyageur",
-      description: "leVoyageur Documentation",
-      contact: {
-        name: "TOTO",
-      },
-      servers: ["http://localhost:3630/"],
-    },
-  },
-  apis: [`src/index.js`, `documentation/*.yaml`],
+	swaggerDefinition: {
+		info: {
+			title: "LeVoyageur",
+			description: "leVoyageur Documentation",
+			contact: {
+				name: "TOTO",
+			},
+			servers: ["http://localhost:3630/"],
+		},
+	},
+	apis: [`src/index.js`, `documentation/*.yaml`],
 };
 const swaggerDocs = swaggerJsDoc(swaggerOptions);
 app.use("/doc", swaggerUI.serve, swaggerUI.setup(swaggerDocs));
@@ -38,46 +38,47 @@ app.use(bodyParser.json());
 
 //************ Middleware de préparation de la requete ************//
 app.use("/api/user/:id/", (req, res, next) => {
-  const userMiddleware = require("../middleware/loadUser");
-  userMiddleware.loadUser(req, res, next);
+	const userMiddleware = require("../middleware/loadUser");
+	userMiddleware.loadUser(req, res, next);
 });
 
 app.use("/api/user/:id/trip/:tripId", (req, res, next) => {
-  const tripMiddleware = require("../middleware/loadTrip");
-  tripMiddleware.loadTrip(req, res, next);
+	const tripMiddleware = require("../middleware/loadTrip");
+	tripMiddleware.loadTrip(req, res, next);
 });
 
 app.use("/api/user/:id/trip/:tripId/step/:stepId", (req, res, next) => {
-  const stepMiddleware = require("../middleware/loadStep");
-  stepMiddleware.loadStep(req, res, next);
+	const stepMiddleware = require("../middleware/loadStep");
+	stepMiddleware.loadStep(req, res, next);
 });
 
 app.use("/api/user/:id/trip/:tripId/ride/:rideId", (req, res, next) => {
-  const rideMiddleware = require("../middleware/loadRide");
-  rideMiddleware.loadRide(req, res, next);
+	const rideMiddleware = require("../middleware/loadRide");
+	rideMiddleware.loadRide(req, res, next);
 });
 
 app.use("/api/user/:id/trip/:tripId/poi/:poiId", (req, res, next) => {
-  const poiMiddleware = require("../middleware/loadPoi");
-  poiMiddleware.loadPoi(req, res, next);
+	const poiMiddleware = require("../middleware/loadPoi");
+	poiMiddleware.loadPoi(req, res, next);
 });
 
 app.use(
-  "/api/user/:id/trip/:tripId/step/:stepId/day/:dayId/",
-  (req, res, next) => {
-    const dayMiddleware = require("../middleware/loadDay");
-    dayMiddleware.loadDay(req, res, next);
-  }
+	"/api/user/:id/trip/:tripId/step/:stepId/day/:dayId/",
+	(req, res, next) => {
+		const dayMiddleware = require("../middleware/loadDay");
+		dayMiddleware.loadDay(req, res, next);
+	}
 );
 
 app.use(
-  "/api/user/:id/trip/:tripId/step/:stepId/day/:dayId/poi/:poiId",
-  (req, res, next) => {
-    const poiMiddleware = require("../middleware/loadPoi");
-    poiMiddleware.loadPoi(req, res, next);
-  }
+	"/api/user/:id/trip/:tripId/step/:stepId/day/:dayId/poi/:poiId",
+	(req, res, next) => {
+		const poiMiddleware = require("../middleware/loadPoi");
+		poiMiddleware.loadPoi(req, res, next);
+	}
 );
-
+console.log(path.join(__dirname, "images"));
+app.use("/images", express.static(path.join(__dirname, "../images")));
 
 //************ Import des routes ************//
 require("../routes")(app);
